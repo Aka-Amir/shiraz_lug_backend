e3ec15f0c31bf76ec701ffaf5a43d1b1893b59d5
Aka-amir <khaliliamir565@gmail.com>
2023-05-29 10:52:25 +0330

    Initial Food commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { UsersModule } from './users/users.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { SettlingModule } from './settling/settling.module';
+import { FoodsModule } from './foods/foods.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/shiraz_lug'), UsersModule, SettlingModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/shiraz_lug'), UsersModule, SettlingModule, FoodsModule],
   controllers: [AppController],
   providers: [],
 })
