0c151730319dd84b253231f0f881ece11af21197
Aka-amir <khaliliamir565@gmail.com>
2023-05-29 10:43:30 +0330

    Initial services commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/shiraz_lug'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/shiraz_lug'), UsersModule, ServicesModule],
   controllers: [AppController],
   providers: [],
 })
