4efd58e378e8cb9cf618ec7c7929c74cd84cc7bf
Aka-amir <khaliliamir565@gmail.com>
2023-06-08 16:43:01 +0330

    New Build

--- src/@core/interceptor/mongoose.interceptor.ts
+++ src/@core/interceptor/mongoose.interceptor.ts
@@ -7,7 +7,6 @@ import {
   HttpException,
   Injectable,
   InternalServerErrorException,
-  Logger,
   NestInterceptor,
   NotFoundException,
 } from '@nestjs/common';
@@ -38,7 +37,7 @@ export class MongooseInterceptor implements NestInterceptor {
         }
 
         if(error instanceof AxiosError) {
-          throw new HttpException(error.response.data, (error.status || 412));
+          throw new HttpException(error.message, (error.status || 412));
         }
         throw InternalServerErrorException;
       }),
